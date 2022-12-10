9cf11dda73b88e54401a2f6b310c5b9c7324e9bd
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2022-12-10 13:19:37 +0200

    +-

---
+++ src/components/ContactFilter/ContactFilter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import css from "../../styles/Common.module.css";
+
+const ContactFilter = ({filter}) => {
+    return (
+    <>
+      <h2>Contacts</h2>
+      <label>Find contacts by name</label>
+      <input 
+        onChange={filter}
+        className={css.input}
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        />
+    </>
+
+    )
+}
+
+ContactFilter.propTypes = {
+  filter: PropTypes.func.isRequired,
+}
+
+export default ContactFilter;
